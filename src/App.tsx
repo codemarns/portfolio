4b491a0431206f72e4df07cc13c14ba661cdf68a
Marnien Cueba <marniencueba94@gmail.com>
2023-10-01 17:01:42 +0800

    add: routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,55 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import React from "react";
+import cn from "classnames";
+import { Routes, Route } from "react-router-dom";
+import { Header, Container, Footer } from "./common";
+import codemarnsLogo from "/codemarns-logo.svg";
+import {
+  Home,
+  About,
+  Landing,
+  Experiences,
+  ComingSoon,
+  NotFound,
+} from "./pages";
+
+const data = {
+  header: {
+    logo: codemarnsLogo,
+    name: ["_code", "marns"],
+    menu: [
+      { path: "/home", page: "home" },
+      { path: "/about", page: "about" },
+      { path: "/landing", page: "landing" },
+      { path: "/experiences", page: "experiences" },
+      { path: "/coming-soon", page: "coming-soon" },
+    ],
+  },
+};
 
 function App() {
-  const [count, setCount] = useState(0);
+  const code = "codemarns";
+  const headerData = data && data.header;
+
+  const mainClasses = cn(
+    code,
+    "w-screen h-screen overflow-x-hidden overflow-y-auto"
+  );
 
   return (
-    <main>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-3xl text-secondary font-bold underline">
-        Vite + React
-      </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <main className={mainClasses}>
+      <Header data={headerData} />
+
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/landing" element={<Landing />} />
+          <Route path="/experiences" element={<Experiences />} />
+          <Route path="/coming-soon" element={<ComingSoon />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Container>
     </main>
   );
 }
