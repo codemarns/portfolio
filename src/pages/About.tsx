3fe57c385d9f0bc7cff20b2e426b665d8c97ed67
Marnien Cueba <marniencueba94@gmail.com>
2023-10-04 05:28:55 +0800

    add: Container in About and Experiences page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,17 +6,19 @@ type Props = React.HTMLAttributes<HTMLDivElement>;
 const About: React.FC<Props> = (props) => {
   const commonArticleClasses = "w-full h-[300px] bg-default-700";
   return (
-    <div {...props} className="ABOUT-PAGE w-full h-auto">
-      <section className="w-full h-[calc(70vh-64px)] bg-default-800">
-        About Section
-      </section>
-      <Container className="xl:container p-4 space-y-4">
-        <article className={commonArticleClasses}>About Article</article>
-        <article className={commonArticleClasses}>About Article</article>
-        <article className={commonArticleClasses}>About Article</article>
-        <article className={commonArticleClasses}>About Article</article>
-      </Container>
-    </div>
+    <Container className="min-h-full">
+      <div {...props} className="ABOUT-PAGE w-full h-auto">
+        <section className="w-full h-[calc(70vh-64px)] bg-default-800">
+          About Section
+        </section>
+        <Container size="xl" paddingX="md" className="space-y-4">
+          <article className={commonArticleClasses}>About Article</article>
+          <article className={commonArticleClasses}>About Article</article>
+          <article className={commonArticleClasses}>About Article</article>
+          <article className={commonArticleClasses}>About Article</article>
+        </Container>
+      </div>
+    </Container>
   );
 };
 
--- src/pages/Experiences.tsx
+++ src/pages/Experiences.tsx
@@ -6,17 +6,27 @@ type Props = React.HTMLAttributes<HTMLDivElement>;
 const Experiences: React.FC<Props> = (props) => {
   const commonArticleClasses = "w-full h-[300px] bg-default-700";
   return (
-    <div {...props} className="EXPERIENCES-PAGE w-full h-auto">
-      <section className="w-full h-[calc(70vh-64px)] bg-default-800">
-        Experiences Section
-      </section>
-      <Container className="xl:container p-4 space-y-4">
-        <article className={commonArticleClasses}>Experiences Article</article>
-        <article className={commonArticleClasses}>Experiences Article</article>
-        <article className={commonArticleClasses}>Experiences Article</article>
-        <article className={commonArticleClasses}>Experiences Article</article>
-      </Container>
-    </div>
+    <Container className="min-h-full">
+      <div {...props} className="EXPERIENCES-PAGE w-full h-auto">
+        <section className="w-full h-[calc(70vh-64px)] bg-default-800">
+          Experiences Section
+        </section>
+        <Container size="xl" paddingX="md" className="space-y-4">
+          <article className={commonArticleClasses}>
+            Experiences Article
+          </article>
+          <article className={commonArticleClasses}>
+            Experiences Article
+          </article>
+          <article className={commonArticleClasses}>
+            Experiences Article
+          </article>
+          <article className={commonArticleClasses}>
+            Experiences Article
+          </article>
+        </Container>
+      </div>
+    </Container>
   );
 };
 
