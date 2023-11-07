81dc9472a8b19adf3caf433a7b46a0100e4c4719
Marnien Cueba <marniencueba94@gmail.com>
2023-11-07 16:49:35 +0800

    Hero section

---
+++ src/sections/Hero/Hero.tsx
@@ -0,0 +1,41 @@
+import { Button } from "../../components";
+import { heroStyles } from "../../styles";
+
+export const Hero = () => {
+  const { root, container, column } = heroStyles;
+
+  return (
+    <section id="hero-section" className={root.base}>
+      <div id="hero-section-container" className={container.base}>
+        <div className={column.left.base}>
+          <div className={column.left.image.wraper}>
+            <img
+              src="marnien-cueba.png"
+              alt="Marnien Cueba Photo"
+              className={column.left.image.base}
+            />
+          </div>
+        </div>
+
+        <div className={column.right.base}>
+          <div className={column.right.details.base}>
+            <span className={column.right.details.greet.base}>Hi, I am</span>
+            <h1 className={column.right.details.name.base}>Marnien Cueba</h1>
+            <p
+              className={column.right.details.title.base}
+              style={{ WebkitTextFillColor: "transparent" }}
+            >
+              UI/UX Designer and Frontend Developer
+            </p>
+          </div>
+          <Button
+            size="xl"
+            label="Hire Me"
+            color="secondary"
+            corner="rounded"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
