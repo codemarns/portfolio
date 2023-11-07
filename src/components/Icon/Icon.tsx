8e5bfb9f8ad7620983e8505bc2d104a262ee779a
Marnien Cueba <marniencueba94@gmail.com>
2023-11-07 21:11:12 +0800

    remove size props in Icon

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,25 +1,15 @@
 import React from "react";
 import cn from "classnames";
 import { iconStyles } from "../../styles";
-import { SizeEnum } from "../../shared";
 
 type TIconProps = {
   className?: string;
   name: string | undefined;
-  size?: keyof typeof SizeEnum;
 };
 
-export const Icon: React.FC<TIconProps> = (props) => {
-  const { className, name, size = "md" } = props;
+export const Icon: React.FC<TIconProps> = ({ className, name }) => {
   const { root } = iconStyles;
   return (
-    <i
-      className={cn(
-        { ["cm-icon-" + name]: name },
-        root.base,
-        root.sizes[size],
-        className
-      )}
-    />
+    <i className={cn({ ["cm-icon-" + name]: name }, root.base, className)} />
   );
 };
