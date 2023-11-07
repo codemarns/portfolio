ff7354e010bb49ffe4948dbdbc965568de124ea0
Marnien Cueba <marniencueba94@gmail.com>
2023-11-07 21:13:49 +0800

    adjust hero layout

--- src/styles/sections/heroStyles.ts
+++ src/styles/sections/heroStyles.ts
@@ -3,11 +3,11 @@ export const heroStyles = {
     base: "h-[99vh] lg:h-fit w-full px-8 py-16 lg:py-20 flex items-center justify-center bg-gradient-to-b from-default-950 to-secondary-900",
   },
   container: {
-    base: "h-fit w-full max-w-[1440px] mx-auto lg:px-8 lg:py-20 grid grid-cols-1 lg:grid-cols-[auto,_1fr] xl:grid-cols-2 gap-0 xl:gap-12 items-center duration-300 ease-out transition-all",
+    base: "h-fit w-full max-w-[1280px] mx-auto lg:px-8 lg:py-20 grid grid-cols-1 lg:grid-cols-[1fr,_552px] xl:grid-cols-2 gap-0 lg:gap-12 items-center duration-300 ease-out transition-all",
   },
   column: {
     left: {
-      base: "p-8 flex items-center justify-center lg:justify-end",
+      base: "p-8 flex items-center justify-center", // lg:justify-end
       image: {
         wraper:
           "relative w-full max-w-[85%] xs:max-w-[250px] md:max-w-[300px] xl:max-w-[350px] inline-flex items-center justify-center p-[10%] xs:p-6 md:p-8 bg-secondary/[0.075] rounded-full before:content-[''] before:absolute before:-inset-10 before:bg-secondary/[0.025] before:rounded-full duration-300 ease-out transition-all",
