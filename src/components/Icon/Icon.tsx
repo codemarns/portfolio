5968ff89242a6d15f0e9bcca60b957da0291d30b
Marnien Cueba <marniencueba94@gmail.com>
2023-11-08 07:48:21 +0800

    add color prop in Icon

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import cn from "classnames";
 import { iconStyles } from "../../styles";
+import { ColorEnum } from "../../shared";
 
 type TIconProps = {
   className?: string;
   name: string | undefined;
+  color?: keyof typeof ColorEnum;
 };
 
-export const Icon: React.FC<TIconProps> = ({ className, name }) => {
+export const Icon: React.FC<TIconProps> = (props) => {
+  const { className, name, color = "default" } = props;
   const { root } = iconStyles;
   return (
-    <i className={cn({ ["cm-icon-" + name]: name }, root.base, className)} />
+    <i
+      className={cn(
+        className,
+        { ["cm-icon-" + name]: name },
+        root.base,
+        root.color[color]
+      )}
+    />
   );
 };
