d6118530482080cf126082767cdd058733a81150
Marnien Cueba <marniencueba94@gmail.com>
2023-11-08 21:55:41 +0800

    export Contacts section

--- src/sections/index.ts
+++ src/sections/index.ts
@@ -2,3 +2,4 @@ export { Hero } from "./Hero/Hero";
 export { About } from "./About/About";
 export { Skills } from "./Skills/Skills";
 export { Experiences } from "./Experiences/Experiences";
+export { Contacts } from "./Contacts/Contacts";
