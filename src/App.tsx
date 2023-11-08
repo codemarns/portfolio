af0a66bcb6d6741510d1e328b8e0e4575bd61d12
Marnien Cueba <marniencueba94@gmail.com>
2023-11-08 09:04:38 +0800

    App: cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-// import { Header } from "./common";
 import { About, Experiences, Hero, Skills } from "./sections";
 import { ScrollToTop } from "./components";
 
 function App() {
   return (
-    <>
-      {/* <Header /> */}
-      <main className="">
-        <Hero />
-        <About />
-        <Skills />
-        <Experiences />
-        <section
-          id="contact"
-          className="h-[700px] w-full px-8 py-20 bg-success"
-        >
-          <div className="w-full h-full bg-white/30">Contacts</div>
-        </section>
-        <ScrollToTop />
-      </main>
-    </>
+    <main className="">
+      <Hero />
+      <About />
+      <Skills />
+      <Experiences />
+      <section id="contact" className="h-[700px] w-full px-8 py-20 bg-success">
+        <div className="w-full h-full bg-white/30">Contacts</div>
+      </section>
+      <ScrollToTop />
+    </main>
   );
 }
 
