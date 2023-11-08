5cc6ae80d104cf70af2a6a4f01e66a0c2d0d0b19
Marnien Cueba <marniencueba94@gmail.com>
2023-11-08 08:34:19 +0800

    Container component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,7 @@
 export { Icon } from "./Icon/Icon";
 export { Button } from "./Button/Button";
 
+export { Section } from "./Section";
+export { Container } from "./Container";
+
 export { ScrollToTop } from "./ScrollToTop";
