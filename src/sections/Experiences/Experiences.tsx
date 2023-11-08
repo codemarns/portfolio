8f9d7e44ecedeeef267d99384fb2f29d7226db97
Marnien Cueba <marniencueba94@gmail.com>
2023-11-08 08:32:18 +0800

    experiences section

---
+++ src/sections/Experiences/Experiences.tsx
@@ -0,0 +1,11 @@
+import { Section } from "../../components";
+
+export const Experiences = () => {
+  const id = "experiences";
+
+  return (
+    <Section id={id} className="bg-success">
+      <div className="w-full h-full bg-white/30">Experiences</div>
+    </Section>
+  );
+};
--- src/sections/index.ts
+++ src/sections/index.ts
@@ -1,3 +1,4 @@
 export { Hero } from "./Hero/Hero";
 export { About } from "./About/About";
 export { Skills } from "./Skills/Skills";
+export { Experiences } from "./Experiences/Experiences";
