1460b2df31a8ec7cc44679bf9200321411b04ea1
Marnien Cueba <marniencueba94@gmail.com>
2023-11-08 08:05:09 +0800

    skills styles

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,5 +1,8 @@
 export { iconStyles } from "./iconStyles";
 export { buttonStyles } from "./buttonStyles";
 
+export { navStyles } from "./sections/navStyles";
+
 export { heroStyles } from "./sections/heroStyles";
 export { aboutStyles } from "./sections/aboutStyles";
+export { skillsStyles } from "./sections/skillsStyles";
