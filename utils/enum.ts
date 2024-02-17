7e4a51353dc659e039d1741de82926a8032dd97b
Marnien Cueba <marniencueba94@gmail.com>
2024-02-17 17:43:44 +0800

    enums

---
+++ utils/enum.ts
@@ -0,0 +1,30 @@
+// COLOR
+export enum ColorEnum {
+  primary,
+  secondary,
+  info,
+  success,
+  warning,
+  danger,
+  default,
+  inherit,
+  black,
+  white,
+}
+export enum DefaultColorEnum {
+  primary = ColorEnum.primary,
+  secondary = ColorEnum.secondary,
+  info = ColorEnum.info,
+  success = ColorEnum.success,
+  warning = ColorEnum.warning,
+  danger = ColorEnum.danger,
+  default = ColorEnum.default,
+}
+
+// SIZE
+export enum SizeEnum {
+  sm,
+  md,
+  lg,
+  xl,
+}
