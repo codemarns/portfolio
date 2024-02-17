a0cadb5b74f418373a0f26a71611f4c15c11a3bb
Marnien Cueba <marniencueba94@gmail.com>
2024-02-17 17:46:50 +0800

    main file for styles

--- styles/index.ts
+++ styles/index.ts
@@ -1,1 +1,10 @@
+// common
 export { headerStyles } from "./common/headerStyles";
+
+// components
+export { iconStyles } from "./components/iconStyles";
+export { buttonStyles } from "./components/buttonStyles";
+export { imageStyles } from "./components/imageStyles";
+
+// sections
+export { heroStyles } from "./sections/heroStyles";
