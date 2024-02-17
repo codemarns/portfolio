be1595994a2df8d661181ef6cc1e78840fbb1e35
Marnien Cueba <marniencueba94@gmail.com>
2024-02-17 17:41:07 +0800

    Header: arrange imports

--- common/header.tsx
+++ common/header.tsx
@@ -1,7 +1,7 @@
-import { headerStyles } from "@/styles";
-import Image from "next/image";
-import Link from "next/link";
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { headerStyles } from "@/styles";
 
 export const Header = () => {
   const menu = [
