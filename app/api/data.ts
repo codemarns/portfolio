85a3051894d0ab8e939614fbd3deb75e97e68711
Marnien Cueba <marniencueba94@gmail.com>
2024-02-17 17:34:57 +0800

    hero api

---
+++ app/api/data.ts
@@ -0,0 +1,13 @@
+export const data = {
+  hero: {
+    img: {
+      alt: "marnien cueba profile",
+      url: "marnien-cueba.png",
+      thumbnail: "", // marnien-cueba.gif
+    },
+    greeting: "Hi, I am",
+    name: "Marnien Cueba",
+    position: "UI/UX and Frontend Developer",
+    cta: "#contacts",
+  },
+};
