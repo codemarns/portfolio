8a592a762c9f7f80781de5048f0a50c6e02d3f59
Marnien Cueba <marniencueba94@gmail.com>
2024-02-17 15:22:52 +0800

    tailwind config > add brand color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,24 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "primary-50": "#F4E8FC",
+        "primary-100": "#E3C9FA",
+        "primary-200": "#D4AAF6",
+        "primary-300": "#C58BF2",
+        "primary-400": "#B66CEE",
+        primary: "#A74DEA",
+        "primary-600": "#8A3FBF",
+        "primary-700": "#6B3095",
+        "primary-800": "#4C216B",
+        "primary-900": "#2D1241",
+        "primary-950": "#0E0317",
+        light: "#F1F5F9",
+        dark: "#1A1A1A",
+      },
+      screens: {
+        xs: "425px",
+        // => @media (min-width: 425px) { ... }
       },
     },
   },
