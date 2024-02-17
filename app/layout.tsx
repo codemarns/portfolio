96e8c0821f4469058795bc0e39ea996059c05c45
Marnien Cueba <marniencueba94@gmail.com>
2024-02-17 17:39:46 +0800

    import icon asset in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Header } from "@/common/header";
 import "./globals.css";
+import "./assets/styles.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
