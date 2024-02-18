6b03463856264391a03b29871b38536a76d38cdb
Marnien Cueba <marniencueba94@gmail.com>
2024-02-18 15:39:44 +0800

    import styles to main index

--- styles/index.ts
+++ styles/index.ts
@@ -1,11 +1,15 @@
 // common
 export { headerStyles } from "./common/headerStyles";
 export { overlayNavStyles } from "./common/overlayNavStyles";
+export { containerStyles } from "./common/containerStyles";
+export { sectionStyles } from "./common/sectionStyles";
 
 // components
 export { iconStyles } from "./components/iconStyles";
 export { buttonStyles } from "./components/buttonStyles";
 export { imageStyles } from "./components/imageStyles";
+export { titleStyles } from "./components/titleStyles";
 
 // sections
 export { heroStyles } from "./sections/heroStyles";
+export { aboutStyles } from "./sections/aboutStyles";
