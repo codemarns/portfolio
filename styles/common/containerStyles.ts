2ac713233266c0bc5ada616fa1e2b5e009395f21
Marnien Cueba <marniencueba94@gmail.com>
2024-02-18 19:58:33 +0800

    size and spacing enhancements

--- styles/common/containerStyles.ts
+++ styles/common/containerStyles.ts
@@ -1,6 +1,6 @@
 export const containerStyles = {
   root: {
-    base: "w-full mx-auto text-center space-y-16 lg:space-y-20",
+    base: "w-full mx-auto text-center py-6 lg:py-8 space-y-16 lg:space-y-20",
     size: {
       sm: "max-w-[640px]",
       md: "max-w-[768px]",
--- styles/common/headerStyles.ts
+++ styles/common/headerStyles.ts
@@ -1,6 +1,6 @@
 export const headerStyles = {
   root: {
-    base: "fixed top-0 left-0 right-0 z-10 h-24 w-full px-8 backdrop-blur-xl bg-black/70 common-transition", // border-b border-secondary
+    base: "fixed top-0 left-0 right-0 z-10 h-16 sm:h-20 md:h-24 w-full px-6 sm:px-8 backdrop-blur-xl bg-black/70 common-transition", // border-b border-secondary
   },
   container: {
     base: "h-full w-full max-w-7xl mx-auto flex items-center justify-between",
