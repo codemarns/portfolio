87180cb2a511795cbde6fb8b322e0536cf53704b
Marnien Cueba <marniencueba94@gmail.com>
2024-02-18 17:59:49 +0800

    Footer section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Header } from "@/common/header/header";
+import { Footer } from "@/common/footer";
 import "./globals.css";
 import "./assets/styles.css";
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ common/footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className="h-16 w-full px-6 border-t border-darker flex items-center justify-center text-light/50 text-sm xs:text-base">
+      © Copyright 2024 Portfolio by codemarns
+    </footer>
+  );
+};
